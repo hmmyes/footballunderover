e5d616b068dd36d4c0c46b5ede27ee88a61e40b6
hmmyes <erkanbayir87@gmail.com>
2025-10-18 18:36:13 +0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Football Under/Over Predictor')
+
+st.write('Welcome to the Football Under/Over Predictor!')
+
+st.write('This app will help predict over/under goals in football matches.')
+
+# Add some sample content
+st.header('Features')
+st.write('- Match analysis')
+st.write('- Historical data')
+st.write('- Predictions')
+
+st.info('This is a basic template. Add your prediction logic here!')
